429c131d037426564e87c6e596ec77c9f5a6b14f
atresanskar <atresanskar55@gmail.com>
2022-04-15 01:11:12 +0530

    show_item_list

---
+++ src/component/Items.js
@@ -0,0 +1,64 @@
+
+const productsArr = [
+
+    {
+    
+    title: 'Colors',
+    
+    price: 100,
+    
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+    
+    },
+    
+    {
+    
+    title: 'Black and white Colors',
+    
+    price: 50,
+    
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+    
+    },
+    
+    {
+    
+    title: 'Yellow and Black Colors',
+    
+    price: 70,
+    
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+    
+    },
+    
+    {
+    
+    title: 'Blue Color',
+    
+    price: 100,
+    
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+    
+    }
+    
+    ]
+    
+    
+
+const Items = () => {
+   const itemList = productsArr.map((item) => (
+       <div>
+          <h2>{item.title}</h2>
+          <h3>${item.price}</h3>
+          <img src={item.imageUrl} alt='item_photo' />
+       </div>
+   ))
+   return (
+       <ul>
+            {itemList}
+       </ul>
+       
+   )
+};
+
+export default Items
